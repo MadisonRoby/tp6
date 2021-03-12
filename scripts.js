90cf2fa179ebc7342351743fe53081fdc4508671
Madison Roby <madison.roby@wsu.edu>
2021-03-12 08:27:17 +0000

    second try

--- scripts.js
+++ scripts.js
@@ -1,46 +1,57 @@
 // JS for TP6
-//AJAX fucntion to load fromFile into object with ID whereTo
+
+// generic AJAX function to load fromFile into object with ID whereTo
 function loadFileInto(fromFile, whereTo) {
-  
-  //create a new XMLHttpRequest object
+
+  // creating a new XMLHttpRequest object
   ajax = new XMLHttpRequest();
-  
-  //defines the GET/POST method, source,, and async value of the AJAX object
+
+  // defines the GET/POST method, source, and async value of the AJAX object
   ajax.open("GET", fromFile, true);
- 
-  //preps code to do something in response to AJAX request
-  ajaz.onreadystatechange = function() {
-    
+
+  // prepares code to do something in response to the AJAX request
+  ajax.onreadystatechange = function() {
+
     if ((this.readyState == 4) && (this.status == 200)) {
-      
+      document.getElementById(whereTo).innerHTML = this.responseText;
+
     } else if ((this.readyState == 4) && (this.status != 200)) {
-      console.log("Error:" + this.responseText);
-    
-  }
-} //end ajax.onreadystatechange
-  
-  //everything is set, initiate request
+      console.log("Error: " + this.responseText);
+
+    }
+
+  } // end ajax.onreadystatechange
+
+  // now that everything is set, initiate request
   ajax.send();
+
 }
 
 window.onload = function() {
-
-loadFileInto("ingredients.html", "ingredients");
-
-loadFileInto("equipment.html", "equipment");
-
-loadFileInto("prep.html", "prep");
-
-loadFileInto("directions.html", "directions");
-};
-
-
-// load the event
-window.onload = function() {
+  
+  loadFileInto("ingredients.html", "ingredients");
+  loadFileInto("prep.html", "prep");
+  loadFileInto("directions.html", "directions");
+  loadFileInto("equipment.html", "equipment");
+  
 
   //changes font size
   document.querySelector("#title").style.fontSize = "100px";
 }
+
+//title color change via click  
+document.querySelector("#title");
+title.onclick = function() {
+  title.classList.toggle("clicked");
+}
+
+// new HTML elements
+x = document.getElementById("enjoy");
+x.innerHTML = "And that's how you make the world's best lasagna! Enjoy!";
+x.style.fontSize = '30px';
+x.style.fontFamily = 'Sigmar One';
+x.style.textAlign = 'center';
+x.style.color = 'saddlebrown';
 
 //defines what's shown on click
 function showIngredients() {
@@ -59,16 +70,3 @@ function showDirections() {
   document.getElementById("content4").style.visibility = "visible";
 }
 
-//title color change via click  
-document.querySelector("#title");
-title.onclick = function() {
-  title.classList.toggle("clicked");
-}
-
-// new HTML elements
-x = document.getElementById("enjoy");
-x.innerHTML = "And that's how you make the world's best lasagna! Enjoy!";
-x.style.fontSize = '30px';
-x.style.fontFamily = 'Sigmar One';
-x.style.textAlign = 'center';
-x.style.color = 'saddlebrown';
